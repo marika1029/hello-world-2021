2ffa9b1b55f8b3a0840c25e45cf328ee8ebc0861
marika1029 <113998022+marika1029@users.noreply.github.com>
2022-09-28 19:53:00 +0200

    novější verze, stále dost neohrabaná, ale snad funguje

--- udaje1.cpp
+++ udaje1.cpp
@@ -14,16 +14,80 @@ std::cout << "Zadej prijmeni: ";
 std::cout << "Zadej vek: ";
     int vek;
     std::cin >> vek;
-    std::cout << "Vek je " << vek << "\n";
 
-std::cout << "Zadej vysku: ";
+    do{
+    do{
+        if(vek<0) {
+            std::cout << "Tak to jsi se jeste nenarodil.\n";
+            std::cout << "Zkus to znovu: ";
+            std::cin >> vek;
+        }
+        else if (vek>120) {
+            std::cout << "Gratuluji k rekordu.\n";
+            std::cout << "Zkus to znova: ";
+            std::cin >> vek;
+        }
+        else {
+            std::cout << "Vas vek je " << vek << "\n";
+        }
+    }while(vek>120);
+    }while(vek<0);
+
+std::cout << "Zadej vysku v cm: ";
     float vyska;
     std::cin >> vyska;
-    std::cout << "Vyska je: " << vyska << "\n";
 
-std::cout << "Zadejte dosazene vzdelani (zs=0; ss=1; vs=2)";
-    std::string skola;
-    std::cin >> skola;
+    do{
+    do{
+        if(vyska < 100) {
+            std::cout << "Tak to jsi mensi nez ja.\n";
+            std::cout << "Zkus to znovu: ";
+            std::cin >> vyska;
+        }
+        else if (vyska>250) {
+            std::cout << "Gratuluji k rekordu bez hrat basketball.\n";
+            std::cout << "Zkus to znova: ";
+            std::cin >> vyska;
+        }
+        else {
+            std::cout << "Vase vyska je " << vyska << "\n";
+        }
+    }while(vyska > 250);
+    }while(vyska < 100);
+
+std::cout << "Zadejte dosazene vzdelani (zs=0, ss=1, vs=2): ";
+    int vzdelani;
+    std::cin >> vzdelani;
+
+    do{
+    do{
+        if(vzdelani == 0) {
+            std::cout << "Vase vzdelani je zakladni.\n";
+        }
+        else if (vzdelani == 1) {
+            std::cout << "Vase vzdelani je stredoskolske.\n";
+        }
+        else if (vzdelani == 2) {
+            std::cout << "Vase vzdelani je vysokoskolske.\n";
+        }
+        else {
+            std::cout << "Zadane vzdelani je naplatne, asi jste martan.\n";
+            std::cout << "Zadejte novy udaj: ";
+            std::cin >> vzdelani;
+        }
+    }while(vzdelani>2);
+    }while(vzdelani<0);
+
+
+    std::cout << "\n";
+    std::cout << "Jmeno: " << jmeno << "\n";
+    std::cout << "Prijmeni: " << prijmeni << "\n";
+    std::cout << "Vek: " << vek << "\n";
+    std::cout << "Vyska: " << vyska << "\n";
+    std::cout << "Vzdelani: " << vzdelani << "(zs=0; ss=1; vs=2)\n";
+
+
+
 
 
 return 0;
